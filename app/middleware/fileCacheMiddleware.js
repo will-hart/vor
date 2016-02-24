73c5ca42a7749ecdb27d19fa9d39843cd0ee3e44
Will Hart <hart.wl@gmail.com>
2016-02-24 11:53:14 +0000

    Set up middleware for writing settings

--- app/middleware/fileCacheMiddleware.js
+++ app/middleware/fileCacheMiddleware.js
@@ -1,16 +1,35 @@
-import { dataPath, saveFile } from '../utils/fileOperations';
+import { dataPath, saveFile, settingsPath } from '../utils/fileOperations';
+
+import { UPDATE_SETTINGS } from '../actions/settings';
 
 /**
- * If requested by users settings, writes the current DB to file
- * as a backup
+ * Writes the current text file and settings to files
  */
 const fileWriterMiddleware = store => next => action => {
-  Promise.resolve().then(() => {
-    const state = store.getState();
-    saveFile(JSON.stringify(state), dataPath);
-  });
+  if (action.type === UPDATE_SETTINGS) {
+    console.log('writing settings');
+    writeSettings(store);
+  } else {
+    console.log('writing md');
+    writeCache(store);
+  }
 
   return next(action);
+};
+
+const writeCache = store => {
+  Promise.resolve().then(() => {
+    const { markdown } = store.getState();
+    saveFile(JSON.stringify(markdown), dataPath);
+  });
+};
+
+const writeSettings = store => {
+  Promise.resolve().then(() => {
+    const { settings } = store.getState();
+    console.log('data is ', settings);
+    saveFile(JSON.stringify(settings), settingsPath);
+  });
 };
 
 export default fileWriterMiddleware;
